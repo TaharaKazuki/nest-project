a1c192cc684aa6b2986dffa6095a9fd1f6012df4
田原 一樹 <kazuki.tahara@rarejob.co.jp>
2021-09-02 10:19:36 +0900

    add service

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,8 @@
 import { Module } from '@nestjs/common';
-import { TasksModule } from './tasks/tasks.module';
+import { TasksController } from './tasks/tasks.controller';
+import { TasksService } from './tasks/tasks.service';
 @Module({
-  imports: [TasksModule],
+  controllers: [TasksController],
+  providers: [TasksService],
 })
 export class AppModule {}
