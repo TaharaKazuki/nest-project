4e03fa7d0833e27b4551960d94a887d5ba3cebbb
Tahara Kazuki <ssp0727.lnc@gmail.com>
2023-08-18 22:21:44 +0900

    update

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,23 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { EventController } from './events.controller';
+import { Event } from './event.entity';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: '127.0.0.1',
+      port: 3306,
+      username: 'root',
+      password: 'example',
+      database: 'nest-events',
+      entities: [Event],
+      synchronize: true,
+    }),
+  ],
   controllers: [AppController, EventController],
   providers: [AppService],
 })
