dc2102ccbf7c4555a0baccb1c531b273a3491a88
Tahara Kazuki <ssp0727.lnc@gmail.com>
2023-08-18 10:13:07 +0900

    refresh

--- src/app.module.ts
+++
@@ -1,8 +0,0 @@
-import { Module } from '@nestjs/common';
-import { TasksController } from './tasks/tasks.controller';
-import { TasksService } from './tasks/tasks.service';
-@Module({
-  controllers: [TasksController],
-  providers: [TasksService],
-})
-export class AppModule {}
